8b63720799c6a9c6edc923483e945afe14cf3dc5
Holger Schmermbeck <hosch@mailbox.org>
2022-12-27 14:23:22 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# courservio-blog
+
+## License
+
+All text and code in this repository is licensed under [CC BY-SA 4.0][].
+
+[cc by-sa 4.0]: https://creativecommons.org/licenses/by-sa/4.0/
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    content: ['./layouts/**/*.html', './content/**/*.md'],
+    theme: {
+        extend: {
+            typography: {
+                DEFAULT: {
+                    css: {
+                        "code::before": {content: ''},
+                        "code::after": {content: ''}
+                    }
+                }
+            }
+        }
+    },
+    plugins: [
+        require('@tailwindcss/typography'),
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/aspect-ratio'),
+    ],
+}
